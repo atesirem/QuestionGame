04e2bb540dcd345e2f27a453351906c86f43bd44
Irem Ates <atesv26@gmail.com>
2020-07-19 18:36:59 +0300

    secondcommit

--- app/src/main/java/com/example/questiongame/data/Data.java
+++ app/src/main/java/com/example/questiongame/data/Data.java
@@ -1,4 +1,5 @@
 package com.example.questiongame.data;
+
 import com.example.questiongame.model.Question;
 
 import java.util.ArrayList;
@@ -6,13 +7,13 @@ import java.util.ArrayList;
 public class Data {
 
     private ArrayList<Question> QuestionDetails = new ArrayList<Question>();
-    Question question1 = new Question("Bana Karşı Her Zaman Kibar Ol","Answer1","Answer2",1,"Bravo","Yazık Kafana",0);
-    Question question2 = new Question("Bana Karşı Her Zaman Kibar Ol","Answer1","Answer2",0,"Bravo","Yazık Kafana",1);
-    Question question3 = new Question("Bana Karşı Her Zaman Kibar Ol","Answer1","Answer2",1,"Bravo","Yazık Kafana",2);
+    Question question1 = new Question("Bana Karşı Her Zaman Kibar Ol", "Answer1", "Answer2", 1, "Bravo", "Yazık Kafana", 0);
+    Question question2 = new Question("Bana Karşı Her Zaman Kibar Ol", "Answer1", "Answer2", 0, "Bravo", "Yazık Kafana", 1);
+    Question question3 = new Question("Bana Karşı Her Zaman Kibar Ol", "Answer1", "Answer2", 1, "Bravo", "Yazık Kafana", 2);
 
 
-
-    public Data(){ }
+    public Data() {
+    }
 
 
 }
